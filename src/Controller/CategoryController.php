c57ffbd17deccf2ae59f91d9123f6054d991d7e2
Eugene Zadorozhnyy <eugene.zadorozhnyi@gns-it.com>
2019-04-02 15:53:19 +0300

    Show posts from the database

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Category\Service\CategoryPresentationServiceInterface;
+use App\Entity\Category;
+use App\Entity\Post;
 use App\Exception\EntityNotFoundException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -25,13 +27,17 @@ final class CategoryController extends AbstractController
     public function show(string $slug): Response
     {
         try {
-            $category = $this->categoryPresentation->findBySlug($slug);
+//            $category = $this->categoryPresentation->findBySlug($slug);
+            $em = $this->getDoctrine()->getManager();
+            $category = $em->getRepository(Category::class)->findBySlug($slug);
+            $post = $em->getRepository(Post::class)->findPostByCategorySlug($slug);
         } catch (EntityNotFoundException $e) {
             throw $this->createNotFoundException('Category not found', $e);
         }
 
         return $this->render('category/show.html.twig', [
             'category' => $category,
+            'posts' => $post
         ]);
     }
 
--- src/Repository/PostRepository.php
+++ src/Repository/PostRepository.php
@@ -19,32 +19,17 @@ class PostRepository extends ServiceEntityRepository
         parent::__construct($registry, Post::class);
     }
 
-    // /**
-    //  * @return Post[] Returns an array of Post objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    /**
+     * @param $slug
+     * @return Post|null
+     */
+    public function findPostByCategorySlug($slug): ?array
     {
         return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('p.id', 'ASC')
-            ->setMaxResults(10)
+            ->leftJoin('p.category', 'c')
+            ->where('c.slug = :slug')
+            ->setParameter('slug', $slug)
             ->getQuery()
-            ->getResult()
-        ;
+            ->getResult();
     }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Post
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
